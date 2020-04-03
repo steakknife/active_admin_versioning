a029aa23267923168d8e426cd7798812950d853f
Alex Mad <lexxeice@gmail.com>
2020-04-03 16:47:24 +0300

    association_tracking v.3

--- lib/active_admin_versioning/active_admin/dsl.rb
+++ lib/active_admin_versioning/active_admin/dsl.rb
@@ -16,7 +16,7 @@ module ActiveAdminVersioning
 
         member_action :rollbacks do
           version = ::PaperTrail::Version.find_by(id: params[:version])
-          if version.reify.save
+          if version.reify(has_many: true, has_one: true).save
             version.destroy
             redirect_to "#{collection_url}/#{params[:id]}",
               notice: I18n.t(:success, default: 'SUCCSESS!', scope: [:active_admin, :versioning])
